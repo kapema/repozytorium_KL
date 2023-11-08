e9edc9218a69e87120408d20ef50a56719132b1a
George <96517504+KacperLewa@users.noreply.github.com>
2023-11-08 12:22:01 +0100

    first commit

---
+++ Desktopowe_KL_4PTN/Notatnik/src/main/java/com/mycompany/notatnik/Notatnik.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.notatnik;
+
+/**
+ *
+ * @author Programista
+ */
+public class Notatnik {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
